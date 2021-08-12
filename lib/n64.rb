4b2603ec56113d17ea7dfa390a509799e5ec657a
mroach <git@c.mroach.com>
2021-08-12 21:44:37 +0200

    chore: remove redundant .rb extension

--- lib/n64.rb
+++ lib/n64.rb
@@ -4,5 +4,5 @@ module N64
   ROM_EXTENSIONS = ["n64", "v64", "z64"].freeze
 end
 
-require_relative "n64/util.rb"
-require_relative "n64/rom_info.rb"
+require_relative "n64/util"
+require_relative "n64/rom_info"
